307eed8226b7741e9bdac77e606be3ec7f365da6
gerrydev <gerrydeveloper66@gmail.com>
2025-03-07 00:42:34 +0700

    fix: 修改基础路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "//web-video-player/",
+  base: "/web-video-player/",
   plugins: [vue()],
 });
